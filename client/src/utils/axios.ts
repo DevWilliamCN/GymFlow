8eddfc2302e007350832ff2c8280c17edbf47ab5
William Cubero Navarro <w.cubero800@gmail.com>
2025-08-04 21:36:16 -0600

    Funcionamiento correcto

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:8080/api'
+    : 'https://gymflow-production.up.railway.app/api';
+
 export const API = axios.create({
-  baseURL: 'https://gymflow-production.up.railway.app/api',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
